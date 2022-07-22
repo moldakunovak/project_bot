b5b09f2cf0e7b30b14b7b01a7b1ec83c481cd642
moldakunovak <kunduz.moldakunova@yandex.ru>
2022-07-22 10:03:29 +0600

    finally_finally

--- exchange_rates.py
+++ exchange_rates.py
@@ -3,60 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 from telebot import types
 from parse import get_parse
-from telebot.types import ReplyKeyboardMarkup
+
 from decouple import config
 
 bot = telebot.TeleBot(config("BOT_TOKEN"))
-
-#
-# @bot.message_handler(commands=["start", "привет"])
-# def get_start_message(message):
-#
-#     text_message = f"Добро пожаловать {message.from_user.first_name} {message.from_user.last_name}!!!"
-#     # bot.send_message(message.chat.id, text_message)
-#     bot.reply_to(message, text_message)
-#
-#
-# @bot.message_handler(content_types=['text'])
-# def eho_message(message):
-#     if message.text.lower() == "lang":
-#         markup = types.InlineKeyboardMarkup(row_width=2)
-#         btn1 = types.InlineKeyboardButton("Python", callback_data="py")
-#         btn2 = types.InlineKeyboardButton("Java", callback_data="java")
-#         btn3 = types.InlineKeyboardButton("JavaScript", callback_data="js")
-#         btn4 = types.InlineKeyboardButton("Matlab", callback_data="mt")
-#         btn5 = types.InlineKeyboardButton("Assembler", url="https://ru.wikipedia.org/wiki/%D0%90%D1%81%D1%81%D0%B5%D0%BC%D0%B1%D0%BB%D0%B5%D1%80")
-#         markup.add(btn1, btn2, btn3, btn4,btn5)
-#     # print(message.text)
-#     mess = f"<i>Выберите язык \n<b>программирования</b>: </i>"
-#     bot.send_message(message.chat.id, mess, parse_mode="HTML", reply_markup=markup)
-#
-# @bot.callback_query_handler(func=lambda call: True)
-# def get_call_data(call):
-#     if call.data == "py":
-#         mess = "<a href='https://www.python.org/'>python</a>"
-#         bot.send_message(call.message.chat.id, mess, parse_mode="HTML")
-#     if call.data == "js":
-#         mess = "<a href='hhttps://learn.javascript.ru/'>java</a>"
-#         bot.send_message(call.message.chat.id, mess, parse_mode="HTML")
-#     if call.data == "java":
-#         mess = "<a href='https://www.java.com/ru/'>java</a>"
-#         bot.send_message(call.message.chat.id, mess, parse_mode="HTML")
-#     if call.data == "mt":
-#         mess = "<a href='https://www.mathworks.com/products/matlab.html'>java</a>"
-#         bot.send_message(call.message.chat.id, mess, parse_mode="HTML")
-#
-# # @bot.message_handler(content_types=['text'])
-# # def get_markup(message):
-# #     if message.text.lower() == "выбор":
-# #         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-# #         btn1 =types.KeyboardButton("yes")
-# #         btn2 =types.KeyboardButton("no")
-# #         markup.add(btn1, btn2)
-# #         bot.send_message(message.chat.id, "выберите:", reply_markup=markup)
-#
-# bot.polling()
-#
 
 
 @bot.message_handler(commands=["start", "привет"])
--- parse.py
+++ parse.py
@@ -4,12 +4,8 @@ from bs4 import BeautifulSoup
 def get_parse():
     URL = 'https://www.nbkr.kg/XML/daily.xml'
     response = requests.get(URL)
-    # print(response)
     soup = BeautifulSoup(response.text, 'xml')
-    # print(soup)
     currency = soup.CurrencyRates.findAll('Currency')
-    # print(currency)
     values = {'USD': currency[0].Value, 'EUR': currency[1].Value,
                 'KZT': currency[2].Value, 'RUB': currency[3].Value}
-    # print(values)
     return values
